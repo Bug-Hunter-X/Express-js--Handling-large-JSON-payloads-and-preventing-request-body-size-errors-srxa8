1cff706113104949e9cd79b16f0e37a4cc831e98
BugHunter-X <askzenai@gmail.com>
2025-01-05 12:20:06 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/users', (req, res) => {
+  // Assume 'users' is an in-memory array for simplicity
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// ... other routes
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
